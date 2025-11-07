6f38ffadaa9ec0b8501372e284a48667d5d39776
jebyrnes <jarrett.byrnes@umb.edu>
2025-11-07 15:09:42 -0500

    big pubs update

---
+++ docs/lab_alums/katy_bland.md
@@ -0,0 +1,10 @@
+---
+title: "Katy Bland"
+image: '/images/students/bland-katy-headshot.jpg'
+date: "2013-03-20"
+categories:
+    - Tech
+include-in-header:
+      text: |
+        <meta http-equiv="refresh" content="0; url=https://www.linkedin.com/in/katy-bland-4a713b42/">
+---
---
+++ publications/_scripts/runme.R
@@ -0,0 +1,23 @@
+# Load the functions
+source("publications/_scripts/bibtex_2_website_qmds.R")
+
+## First run for 20251030 library
+# Run and format library
+# bibtex_2_website_qmds(
+#     bibfile = "publications/_zotero_outputs/20251030_zotero_initial_output/JEKB_publications.bib",
+#     pdf_files_folder_location = "publications/_zotero_outputs/20251030_zotero_initial_output",
+#     outfold = "publications/articles/",
+#     overwrite = FALSE
+# )
+
+
+#add
+
+# bibtex_2_website_qmds(
+#     bibfile = "publications/_zotero_outputs/vonshuckman/vonshuckman.bib",
+#     pdf_files_folder_location = "publications/_zotero_outputs/vonshuckman",
+#     outfold = "publications/articles/",
+#     overwrite = FALSE
+# )
+
+
